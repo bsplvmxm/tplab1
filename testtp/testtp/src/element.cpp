f46f1e73467071f9d7301f1fd4593b2ead6da56a
bsplvmxm <i.maksim_01@mail.ru>
2021-10-29 13:35:02 +0300

    element.cpp, изменение в коде

--- testtp/testtp/src/element.cpp
+++ testtp/testtp/src/element.cpp
@@ -3,4 +3,5 @@
 garage* element::get_obj() { return obj; }
 element* element::get_prev() { return prev; }
 
-
+void element::set_obj(garage* Obj) { obj = Obj; }
+void element::set_prev(element* Prev) { prev = Prev; }
